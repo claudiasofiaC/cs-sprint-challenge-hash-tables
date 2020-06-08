1e9bbd81c4a03261849eb2c9c690be9c9c2d8c93
Claudia Chajon <53544970+claudiasofiaC@users.noreply.github.com>
2020-06-08 09:58:55 -0400

    Add files via upload

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    pos = {}
+    neg = {}
+    result = []
+
+    for num in a:
+        if num > 0:
+            pos[num] = -num
+        if num < 0:
+            neg[num] = num * 1
+    for key in pos:
+        if pos[key] in neg:
+            result.append(key)
 
     return result
 
