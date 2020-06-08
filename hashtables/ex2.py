ef84fed8703920f18069f90d893abc1e00a1c44d
Claudia Chajon <53544970+claudiasofiaC@users.noreply.github.com>
2020-06-08 09:56:48 -0400

    Add files via upload

---
+++ hashtables/ex2.py
@@ -0,0 +1,25 @@
+#  Hint:  You may not need all of these.  Remove the unused functions.
+class Ticket:
+    def __init__(self, source, destination):
+        self.source = source
+        self.destination = destination
+
+
+def reconstruct_trip(tickets, length):
+    """
+    YOUR CODE HERE
+    """
+    # Your code here
+
+    dic = {}
+    route = []
+    for ticket in tickets:
+        dic[ticket.source] = ticket.destination
+
+    cur = dic["NONE"]
+    route.append(cur)
+    while cur != "NONE":
+        cur = dic[cur]
+        route.append(cur)
+
+    return route
---
+++ hashtables/ex3.py
@@ -0,0 +1,31 @@
+def intersection(arrays):
+    """
+    YOUR CODE HERE
+    """
+    dic = {}
+    result = []
+
+    for num in arrays[0]:
+        dic[num] = 1
+
+    for array in arrays[1:]:
+        for num in array:
+            if num in dic:
+                dic[num] += 1
+
+    for key in dic:
+        if dic[key] == len(arrays):
+            result.append(key)
+
+
+    return result
+
+
+if __name__ == "__main__":
+    arrays = []
+
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+    print(intersection(arrays))
